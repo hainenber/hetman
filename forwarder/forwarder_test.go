def70eb2199c553eb91c4244967b93fb47c4ef1e
hainenber <dotronghai96@gmail.com>
2023-03-19 17:28:38 +0700

    feat(*): add placeholder test funcs across modules

--- forwarder/forwarder_test.go
+++ forwarder/forwarder_test.go
@@ -1,1 +1,15 @@
 package forwarder
+
+import "testing"
+
+func TestNewForwarder(t *testing.T) {
+
+}
+
+func TestRun(t *testing.T) {
+
+}
+
+func TestForward(t *testing.T) {
+
+}
---
+++ tailer/tailer_test.go
@@ -0,0 +1,12 @@
+package tailer
+
+import "testing"
+
+func TestNewTailer(t *testing.T) {
+}
+
+func TestRegisterForwarder(t *testing.T) {
+}
+
+func TestTail(t *testing.T) {
+}
