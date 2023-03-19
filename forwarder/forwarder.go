683957a91d353b8ebaca5edc3f8b39ca771c302a
hainenber <dotronghai96@gmail.com>
2023-03-19 17:30:48 +0700

    feat(forwarder): * separate forwarder for each target * allow forwarding dynamic labels with logs * check resp's status after forwarding to remote

--- forwarder/forwarder.go
+++ forwarder/forwarder.go
@@ -3,33 +3,63 @@ package forwarder
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
+	"os"
 	"time"
+
+	"github.com/hainenber/hetman/config"
+	"github.com/rs/zerolog"
 )
 
-type PayloadStreamContent struct {
-	Label string `json:"label"`
-}
-
 type PayloadStream struct {
-	Stream PayloadStreamContent `json:"stream"`
-	Values [][]string           `json:"values"`
+	Stream map[string]string `json:"stream"`
+	Values [][]string        `json:"values"`
 }
 
 type Payload struct {
 	Streams []PayloadStream `json:"streams"`
 }
 
-func Forward(url, logLine string) error {
+type Forwarder struct {
+	conf    *config.ForwarderConfig
+	LogChan chan string
+	logger  zerolog.Logger
+}
+
+func NewForwarder(conf config.ForwarderConfig) *Forwarder {
+	return &Forwarder{
+		conf:    &conf,
+		LogChan: make(chan string),
+		logger:  zerolog.New(os.Stdout),
+	}
+}
+
+func (f *Forwarder) Run() {
+	go func() {
+		for {
+			fmt.Println("goroutine is still running")
+			line := <-f.LogChan
+			if f.conf.URL != "" {
+				err := f.Forward(f.conf.URL, line)
+				if err != nil {
+					f.logger.Error().Err(err).Msg("")
+				}
+			}
+		}
+	}()
+}
+
+func (f *Forwarder) Forward(url, logLine string) error {
 	client := &http.Client{}
 
-	// TODO: Fetch label from config
+	// Fetch tags from config
 	// TODO: Send logs in batches
 	payload, err := json.Marshal(Payload{
 		Streams: []PayloadStream{
 			{
-				Stream: PayloadStreamContent{Label: "hetman"},
+				Stream: f.conf.AddTags,
 				Values: [][]string{
 					{fmt.Sprint(time.Now().UnixNano()), logLine},
 				},
@@ -40,16 +70,18 @@ func Forward(url, logLine string) error {
 		return err
 	}
 
+	// Initalize HTTP client for POST request to log servers
+	// Since we're sending data as JSON data, the header must be set as well
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
 	if err != nil {
 		return err
 	}
 	req.Header.Add("Content-Type", "application/json")
 
-	fmt.Println(req.Body)
-
-	_, err = client.Do(req)
-	// fmt.Println(resp.StatusCode)
+	resp, err := client.Do(req)
+	if err == nil && resp.StatusCode != 204 {
+		err = errors.New("unexpected status code from log server")
+	}
 
 	return err
 }
