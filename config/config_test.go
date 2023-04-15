63bf6abbc0e61605773dba501f31d0ac9fc75389
hainenber <dotronghai96@gmail.com>
2023-04-15 13:37:34 +0700

    refactor(test): own func to prepare sample conf

--- config/config_test.go
+++ config/config_test.go
@@ -7,6 +7,29 @@ import (
 	"testing"
 )
 
+func prepareTestConfig() (*Config, []string) {
+	tmpDir, _ := os.MkdirTemp("", "test_translate_wildcards")
+	defer os.RemoveAll(tmpDir)
+
+	fnames := make([]string, 2)
+	for i := range make([]bool, 2) {
+		fname := filepath.Join(tmpDir, fmt.Sprintf("file%v.log", i))
+		os.WriteFile(fname, []byte{1, 2}, 0666)
+		fnames[i] = fname
+	}
+
+	globTmpDir := filepath.Join(tmpDir, "*.log")
+	conf := &Config{
+		Targets: []TargetConfig{
+			{
+				Paths: []string{globTmpDir},
+			},
+		},
+	}
+
+	return conf, fnames
+}
+
 func TestNewConfig(t *testing.T) {
 	conf, err := NewConfig("hetman.yaml.example")
 	if err != nil {
@@ -38,31 +61,14 @@ func TestNewConfig(t *testing.T) {
 }
 
 func TestTranslateWildcards(t *testing.T) {
-	tmpDir, _ := os.MkdirTemp("", "test_translate_wildcards")
-	defer os.RemoveAll(tmpDir)
+	conf, fnames := prepareTestConfig()
 
-	fnames := make([]string, 2)
-	for i := range make([]bool, 2) {
-		fname := filepath.Join(tmpDir, fmt.Sprintf("file%v.log", i))
-		os.WriteFile(fname, []byte{1, 2}, 0666)
-		fnames[i] = fname
-	}
-
-	globTmpDir := filepath.Join(tmpDir, "*.log")
-	conf := &Config{
-		Targets: []TargetConfig{
-			{
-				Paths: []string{globTmpDir},
-			},
-		},
-	}
-
-	newConf, err := conf.TranslateWildcards()
+	err := conf.TranslateWildcards()
 	if err != nil {
 		t.Errorf("expect nil, got %v", err)
 	}
 
-	for _, target := range newConf.Targets {
+	for _, target := range conf.Targets {
 		for _, path := range target.Paths {
 			if path != fnames[0] && path != fnames[1] {
 				t.Errorf("expect %v or %v, got %v", fnames[0], fnames[1], path)
@@ -86,5 +92,13 @@ func TestDetectDuplicateTargetID(t *testing.T) {
 	err := conf.DetectDuplicateTargetID()
 	if err == nil {
 		t.Errorf("expect %v, got nil", err)
+	}
+}
+
+func TestTransformAndValidate(t *testing.T) {
+	conf, _ := prepareTestConfig()
+	_, err := conf.ValidateAndTransform()
+	if err != nil {
+		t.Errorf("expect nil, got %v", err)
 	}
 }
