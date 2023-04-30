98c62b7eee24d1ad5371ec1392189f253d0f5864
hainenber <dotronghai96@gmail.com>
2023-04-30 21:34:11 +0700

    feat(fwd): add happy unit tests for fwd methods

--- forwarder/forwarder_test.go
+++ forwarder/forwarder_test.go
@@ -1,15 +1,65 @@
 package forwarder
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-func TestNewForwarder(t *testing.T) {
+	"github.com/hainenber/hetman/config"
+	"github.com/stretchr/testify/assert"
+)
 
+func prepareTestForwarder(urlOverride string) *Forwarder {
+	fwdCfg := config.ForwarderConfig{
+		URL:     "http://localhost:8088",
+		AddTags: map[string]string{"foo": "bar"},
+	}
+	if urlOverride != "" {
+		fwdCfg.URL = urlOverride
+	}
+	return NewForwarder(fwdCfg)
 }
 
-func TestRun(t *testing.T) {
+func TestNewForwarder(t *testing.T) {
+	fwd := prepareTestForwarder("")
+	assert.NotNil(t, fwd)
+	assert.Equal(t, 0, cap(fwd.LogChan))
+	assert.Equal(t, "687474703a2f2f6c6f63616c686f73743a38303838666f6f626172", fwd.Signature)
+}
 
+func TestCreateForwarderSignature(t *testing.T) {
+	fwdCfg := config.ForwarderConfig{
+		URL:     "http://localhost:8088",
+		AddTags: map[string]string{"a": "b", "foo": "bar"},
+	}
+	assert.Equal(t, "687474703a2f2f6c6f63616c686f73743a3830383861666f6f62626172", CreateForwarderSignature(fwdCfg))
+}
+
+func TestForwarderFlush(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "")
+	}))
+	defer server.Close()
+
+	fwd := prepareTestForwarder(server.URL)
+
+	go func() {
+		fwd.LogChan <- "abc"
+	}()
+
+	err := fwd.Flush()
+	assert.Nil(t, err)
 }
 
 func TestForward(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "")
+	}))
+	defer server.Close()
 
+	fwd := prepareTestForwarder(server.URL)
+
+	err := fwd.Forward("", "abc")
+	assert.Nil(t, err)
 }
