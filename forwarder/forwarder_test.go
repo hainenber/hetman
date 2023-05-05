6d9f3c849e50dbd1a82c9059106a5cc737f40979
hainenber <dotronghai96@gmail.com>
2023-05-05 15:30:03 +0700

    feat(fwd): unit test for fwd's run method

--- forwarder/forwarder_test.go
+++ forwarder/forwarder_test.go
@@ -1,9 +1,10 @@
 package forwarder
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"sync"
 	"testing"
 
 	"github.com/hainenber/hetman/config"
@@ -28,30 +29,98 @@ func TestNewForwarder(t *testing.T) {
 	assert.Equal(t, "687474703a2f2f6c6f63616c686f73743a38303838666f6f626172", fwd.Signature)
 }
 
-func TestForwarderFlush(t *testing.T) {
+func TestForwarderRun(t *testing.T) {
+	var (
+		reqCount int
+		wg       sync.WaitGroup
+	)
+
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "")
+		payload := Payload{}
+		if reqCount == 0 {
+			json.NewDecoder(r.Body).Decode(&payload)
+			assert.Contains(t, payload.Streams[0].Values[0], "success")
+			reqCount++
+			return
+		}
+		if reqCount == 1 {
+			w.WriteHeader(http.StatusInternalServerError)
+			reqCount++
+		}
 	}))
 	defer server.Close()
 
 	fwd := prepareTestForwarder(server.URL)
+	bufferChan := make(chan string, 1)
 
 	go func() {
-		fwd.LogChan <- "abc"
+		fwd.LogChan <- "success"
+		fwd.LogChan <- "failed"
+		close(fwd.LogChan)
 	}()
 
-	err := fwd.Flush()
-	assert.Nil(t, err)
+	wg.Add(1)
+	fwd.Run(&wg, bufferChan)
+	fwd.Close()
+
+	wg.Wait()
+
+	assert.Equal(t, "failed", <-bufferChan)
+}
+
+func TestForwarderFlush(t *testing.T) {
+	var reqCount int
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		payload := Payload{}
+		if reqCount == 0 {
+			json.NewDecoder(r.Body).Decode(&payload)
+			assert.Contains(t, payload.Streams[0].Values[0], "success")
+			reqCount++
+			return
+		}
+		if reqCount == 1 {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	}))
+	defer server.Close()
+
+	fwd := prepareTestForwarder(server.URL)
+	bufferChan := make(chan string, 1)
+
+	go func() {
+		fwd.LogChan <- "success"
+		fwd.LogChan <- "failed"
+		close(fwd.LogChan)
+	}()
+
+	errors := fwd.Flush(bufferChan)
+	assert.Len(t, errors, 1)
+	assert.Equal(t, "failed", <-bufferChan)
 }
 
 func TestForward(t *testing.T) {
+	var reqCount int
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "")
+		payload := Payload{}
+
+		if reqCount == 0 {
+			json.NewDecoder(r.Body).Decode(&payload)
+			assert.Contains(t, payload.Streams[0].Values[0], "abc")
+			reqCount++
+			return
+		}
+
+		json.NewDecoder(r.Body).Decode(&payload)
+		assert.Equal(t, []string{"123", "def"}, payload.Streams[0].Values[0])
 	}))
 	defer server.Close()
 
 	fwd := prepareTestForwarder(server.URL)
 
 	err := fwd.Forward("", "abc")
+	assert.Nil(t, err)
+
+	err = fwd.Forward("123", "def")
 	assert.Nil(t, err)
 }
