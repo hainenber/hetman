6a59d63002d43a82aa92be1d111e23cfdabce656
hainenber <dotronghai96@gmail.com>
2023-06-10 20:13:06 +0700

    feat(parser): add log parser component

---
+++ internal/parser/parser.go
@@ -0,0 +1,149 @@
+package parser
+
+import (
+	"context"
+
+	"github.com/hainenber/hetman/internal/pipeline"
+	"github.com/hainenber/hetman/internal/telemetry/metrics"
+	"github.com/jeromer/syslogparser"
+	"github.com/jeromer/syslogparser/rfc3164"
+	"github.com/jeromer/syslogparser/rfc5424"
+	"github.com/rs/zerolog"
+	"github.com/satyrius/gonx"
+	"github.com/valyala/fastjson"
+)
+
+type Parser struct {
+	cancelFunc context.CancelFunc
+	ctx        context.Context
+
+	format      string
+	pattern     string
+	logger      zerolog.Logger
+	nginxParser *gonx.Parser
+	jsonParser  fastjson.Parser
+
+	ParserChan chan pipeline.Data
+}
+
+type ParserOptions struct {
+	Pattern string
+	Format  string
+	Logger  zerolog.Logger
+}
+
+func NewParser(opts ParserOptions) *Parser {
+
+	ctx, cancelFunc := context.WithCancel(context.Background())
+
+	metrics.Meters.InitializedComponents["parser"].Add(ctx, 1)
+
+	parser := &Parser{
+		cancelFunc: cancelFunc,
+		ctx:        ctx,
+		format:     opts.Format,
+		pattern:    opts.Pattern,
+		logger:     opts.Logger,
+		ParserChan: make(chan pipeline.Data, 1024),
+	}
+
+	switch opts.Format {
+	case "nginx":
+		parser.nginxParser = gonx.NewParser(opts.Pattern)
+	case "json":
+		parser.jsonParser = fastjson.Parser{}
+	}
+
+	return parser
+}
+
+func (p *Parser) Run(bufferChans []chan pipeline.Data) {
+	for {
+		select {
+		case <-p.ctx.Done():
+			return
+		case data := <-p.ParserChan:
+			switch p.format {
+			case "nginx":
+				parsed, err := p.nginxParser.ParseString(data.LogLine)
+				if err != nil {
+					p.logger.Error().Err(err).Msg("")
+					break
+				}
+				if parsed == nil {
+					p.logger.Error().Msg("Parsed Nginx log is nil")
+					break
+				}
+				data.Parsed = make(map[string]string, len(parsed.Fields()))
+				for k, v := range parsed.Fields() {
+					data.Parsed[k] = v
+				}
+			case "json":
+				parsed, err := p.jsonParser.Parse(data.LogLine)
+				if err != nil {
+					p.logger.Error().Err(err).Msg("")
+					break
+				}
+				if parsed == nil {
+					p.logger.Error().Msg("Parsed JSON log is nil")
+					break
+				}
+				parsedJson, err := getKeyValuePairs(parsed)
+				if err != nil {
+					p.logger.Error().Err(err).Msg("Cannot parse JSON log further")
+					break
+				}
+				data.Parsed = parsedJson
+			case "syslog-rfc5424", "syslog-rfc3164":
+				var syslogParser syslogparser.LogParser
+				if p.format == "syslog-rfc5424" {
+					syslogParser = rfc5424.NewParser([]byte(data.LogLine))
+				} else if p.format == "syslog-rfc3164" {
+					syslogParser = rfc3164.NewParser([]byte(data.LogLine))
+				}
+				if err := syslogParser.Parse(); err != nil {
+					p.logger.Error().Err(err).Msg("")
+					break
+				}
+				parsed := syslogParser.Dump()
+				data.Parsed = make(map[string]string, len(parsed))
+				for k, v := range parsed {
+					if strV, ok := v.(string); ok {
+						data.Parsed[k] = strV
+					}
+				}
+			}
+
+			// Broadcast parsed log to buffers if there are multiple sinks (forwarders)
+			for _, bufferChan := range bufferChans {
+				bufferChan <- data
+			}
+		default:
+			continue
+		}
+	}
+}
+
+func (p *Parser) Close() {
+	metrics.Meters.InitializedComponents["parser"].Add(p.ctx, -1)
+
+	p.cancelFunc()
+}
+
+func getKeyValuePairs(val *fastjson.Value) (map[string]string, error) {
+	o, err := val.Object()
+	if err != nil {
+		return nil, err
+	}
+
+	kvPairs := make(map[string]string, o.Len())
+	o.Visit(func(k []byte, v *fastjson.Value) {
+		strV, err := v.StringBytes()
+		if err != nil {
+			return
+		}
+		kvPairs[string(k)] = string(strV)
+	})
+
+	return kvPairs, nil
+}
