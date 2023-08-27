c551a185b60c789aa3b6deaa36e31b0fd8c50594
hainenber <dotronghai96@gmail.com>
2023-08-27 19:48:22 +0700

    feat(testing): trick to exec tests need to access relpath

---
+++ testing/testing.go
@@ -0,0 +1,17 @@
+package testing
+
+import (
+	"os"
+	"path"
+	"runtime"
+)
+
+// Reference: https://intellij-support.jetbrains.com/hc/en-us/community/posts/360009685279-Go-test-working-directory-keeps-changing-to-dir-of-the-test-file-instead-of-value-in-template?page=1#community_comment_360002183640
+func init() {
+	_, filename, _, _ := runtime.Caller(0)
+	dir := path.Join(path.Dir(filename), "..")
+	err := os.Chdir(dir)
+	if err != nil {
+		panic(err)
+	}
+}
