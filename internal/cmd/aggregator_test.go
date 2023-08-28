d7bab61d3ce8fde3843b2ecd95fb3966f0836c81
hainenber <dotronghai96@gmail.com>
2023-08-28 22:59:40 +0700

    feat(cmd/agg): unit test for running agg server

--- internal/cmd/aggregator_test.go
+++ internal/cmd/aggregator_test.go
@@ -4,27 +4,29 @@ import (
 	"bytes"
 	"compress/gzip"
 	"encoding/json"
+	"net"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/hainenber/hetman/internal/forwarder"
 	"github.com/hainenber/hetman/internal/pipeline"
+	_ "github.com/hainenber/hetman/testing"
+	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/assert"
 )
 
 func sendTestLogPayload(inputChans []chan pipeline.Data, compressed bool, data any) (*httptest.Server, *http.Response, error) {
-	var (
-		jsonContentHeader = "application/json"
-	)
+	var payload *bytes.Buffer
 	logPayloadReceiverHandler := receiveLogPayload(inputChans)
 
 	aggregatorServer := httptest.NewServer(
 		http.HandlerFunc(logPayloadReceiverHandler),
 	)
 
-	var payload *bytes.Buffer
 	marshalledPayload, _ := json.Marshal(data)
 	if data == nil {
 		payload = nil
@@ -45,7 +47,7 @@ func sendTestLogPayload(inputChans []chan pipeline.Data, compressed bool, data a
 		aggregatorServer.URL, payload,
 	)
 
-	req.Header.Set("Content-Type", jsonContentHeader)
+	req.Header.Set("Content-Type", "application/json")
 	if compressed && payload != nil {
 		req.Header.Set("Content-Encoding", "gzip")
 	}
@@ -113,5 +115,61 @@ func TestReceiveLogPayload(t *testing.T) {
 }
 
 func TestAggregatorRun(t *testing.T) {
-	// TODO
+	t.Run("aggregator should run, relay sent payload and close when received SIGTERM signal", func(t *testing.T) {
+		var (
+			wg                sync.WaitGroup
+			once              sync.Once
+			doneForwardedChan = make(chan bool)
+		)
+		// Create test forwarder server
+		forwarder := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			once.Do(func() {
+				doneForwardedChan <- true
+			})
+		}))
+		listener, err := net.Listen("tcp", "localhost:50000")
+		if err != nil {
+			log.Error().Err(err).Msg("")
+			os.Exit(1)
+		}
+		forwarder.Listener.Close()
+		forwarder.Listener = listener
+		forwarder.Start()
+		defer forwarder.Close()
+
+		agg := Aggregator{
+			ConfigFile: "internal/cmd/testdata/hetman.aggregator.yaml",
+			Port:       50001,
+		}
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			agg.Run()
+		}()
+
+		// Wait until aggregator's port is ready
+		conn, err := net.DialTimeout("tcp", net.JoinHostPort("localhost", "50001"), time.Second)
+		for err != nil || conn == nil {
+			conn, err = net.DialTimeout("tcp", net.JoinHostPort("localhost", "50001"), time.Second)
+		}
+		conn.Close()
+
+		// Send a test payload
+		req, _ := http.NewRequest(
+			http.MethodPost,
+			"http://localhost:50001/logs",
+			bytes.NewBufferString("{\"a\":\"b\"}"),
+		)
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := http.DefaultClient.Do(req)
+		assert.Nil(t, err)
+		assert.Equal(t, 200, resp.StatusCode)
+
+		<-doneForwardedChan
+		agg.Close()
+
+		wg.Wait()
+	})
 }
